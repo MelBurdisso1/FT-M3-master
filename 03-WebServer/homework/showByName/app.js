258681e0d68b2b074d8350d114a539320cb3b29f
MelBurdisso1 <missmelburdisso@gmail.com>
2022-11-03 16:25:40 -0300

    hecho

--- 03-WebServer/homework/showByName/app.js
+++ 03-WebServer/homework/showByName/app.js
@@ -1,4 +1,17 @@
 var fs  = require("fs")
 var http  = require("http")
 
+
 // Escribí acá tu servidor
+http.createServer(function(req,res){
+   fs.readFile(`./images${req.url}.jpg`), function(err,data){
+    if(err){
+        fs.writeHead(404, {'Content-type': 'text/plain'})
+        fs.end('image not found')
+    } else {
+        fs.writeHead(200, {'Content-type':'image/jpeg'})
+        fs.end(data)
+    }
+   }
+   }
+).listen(3000,'127.0.0.1')
