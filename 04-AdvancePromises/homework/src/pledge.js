f45f2e4d1fb81f3e6f901df890bb7b644a2166c4
MelBurdisso1 <missmelburdisso@gmail.com>
2022-11-05 13:55:20 -0300

    hecho con test no pasando incompleto

--- 04-AdvancePromises/homework/src/pledge.js
+++ 04-AdvancePromises/homework/src/pledge.js
@@ -1,10 +1,57 @@
-'use strict';
+"use strict";
 /*----------------------------------------------------------------
 Promises Workshop: construye la libreria de ES6 promises, pledge.js
 ----------------------------------------------------------------*/
 // // TU CÓDIGO AQUÍ:
+function $Promise(executor) {
+  if (typeof executor !== "function")
+    throw new TypeError("Executor must be a function");
 
+  this._state = "pending";
 
+  executor(this._internalResolve.bind(this), this._internalReject.bind(this));
+}
+
+$Promise.prototype._internalResolve = function (data) {
+  if (this._state === "pending") {
+    this._state = "fulfilled";
+    this._value = data;
+    this._callHandlers();
+  }
+};
+
+$Promise.prototype._internalReject = function (reason) {
+  if (this._state === "pending") {
+    this._state = "rejected";
+    this._value = reason;
+    this._callHandlers();
+  }
+};
+
+$Promise.prototype.then = function (successCb, errorCb) {
+  if (typeof successCb !== "function") successCb = false;
+  if (typeof errorCb !== "function") errorCb = false;
+  const downstreamPromise= new $Promise(function(){})
+  this._handlerGroups.push({ successCb, errorCb, downstreamPromise});
+  if (this._state !== "pending") {
+    this._callHandlers();
+  }
+  return downstreamPromise;
+};
+
+$Promise.prototype._callHandlers = function () {
+  while (this._handlerGroups.length) {
+    let callback = this._handlerGroups.shift();
+
+    if (this._state === "fulfilled") {
+      callback.successCb && callback.successCb(this._value);
+    } else callback.errorCb && callback.errorCb(this._value);
+  }
+};
+
+$Promise.prototype.catch = function (errorCb) {
+  this.then(null, errorCb);
+};
 
 module.exports = $Promise;
 /*-------------------------------------------------------
